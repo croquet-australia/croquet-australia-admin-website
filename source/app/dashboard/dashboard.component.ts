5ee5202a0e2646cd78df59bcf3d2dc6762dfe2dd
Tim Murphy <tim@26tp.com>
2016-03-07 05:11:15 +1100

    Improve dashboard

--- source/app/dashboard/dashboard.component.ts
+++ source/app/dashboard/dashboard.component.ts
@@ -1,22 +1,13 @@
 import { Component, OnInit } from 'angular2/core';
 import { Router } from 'angular2/router';
-import { Tournament, TournamentsService } from '../tournaments/tournaments.service';
 
 @Component({
     selector: 'ca-dashboard',
     templateUrl: 'app/dashboard/dashboard.component.html',
     styleUrls: ['app/dashboard/dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-    public tournaments: Tournament[] = [];
 
-    constructor(private _tournamentsService: TournamentsService, private _router: Router) { }
-
-    ngOnInit() {
-        this._tournamentsService.getTournaments().then(tournaments => this.tournaments = tournaments.slice(1, 5));
-    }
-
-    gotoDetail(tournament: Tournament) {
-        this._router.navigate(['Tournament', { id: tournament.id }]);
+export class DashboardComponent {
+    constructor(public router: Router) {
     }
 }
