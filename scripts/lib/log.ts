d23d6a370ff2051c7c0785cb21349c1f5fee6021
Tim Murphy <tim@26tp.com>
2016-09-09 19:30:22 +1000

    Install library scripts

---
+++ scripts/lib/log.ts
@@ -0,0 +1,29 @@
+/// <reference path='../../typings/index.d.ts' />
+
+import * as chalk  from 'chalk';
+
+export class Log {
+    public debug(message: string): void {
+        this.writeLine(chalk.gray(message));
+    }
+
+    public error(message: string): void {
+        this.writeLine(chalk.red(message));
+    }
+    
+    public info(message: string): void {
+        this.writeLine(message);
+    }
+
+    public highlight(message: any): string {
+        return chalk.yellow(message);
+    }
+
+    public warn(message: string): void {
+        this.writeLine(chalk.yellow(message));
+    }
+
+    private writeLine(message: string): void {
+        console.log(message);
+    }
+}
