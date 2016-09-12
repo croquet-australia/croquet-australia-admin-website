6da95c16ddc5ef56bd1abb4cbd795c1d41fef64a
Tim Murphy <tim@26tp.com>
2016-09-13 06:40:17 +1000

    build: add scripts to create Azure website

---
+++ config.ts
@@ -0,0 +1,64 @@
+/**
+ * Global configuration information for code and scripts in this repository.
+ * 
+ * Azure Management Certificate
+ * ============================
+ * config.azure.getCertificateCredentials() depends on the Azure management certificate being
+ * installed on this machine. 
+ * 
+ * How to install Azure Management Certificate
+ * -------------------------------------------
+ * - Install Azure CLI
+ * 
+ *      cmd> npm install --global azure-cli
+ * 
+ * - Download Azure publishsettings file from http://go.microsoft.com/fwlink/?LinkId=254432
+ * 
+ * - Import Azure publishsettings file
+ * 
+ *      cmd> azure account import [file name].publishsettings
+ * 
+ * - Export Azure Management Certificate
+ * 
+ *      cmd> azure account cert export --subscription [azure subscription id] --file %USERPROFILE%\.secrets\azure\[azure subscription id].pem
+ * 
+ * Note: the above commands are run with Windows cmd.exe. The same commands, with minor modifications should
+ * run on Mac or Linux.
+ * 
+ * todo: replace azure account commands with a script. 
+ */
+
+/// <reference path="typings/index.d.ts" />
+
+const fs = require('fs');
+const os = require('os');
+const webSiteManagement = require('azure-mgmt-website');
+
+class Config {
+    azure = {
+        pem: `${os.homedir()}\\.secrets\\azure\\32e22437-24a9-4824-9144-c3057f8bd439.pem`,
+        subscriptionId: `32e22437-24a9-4824-9144-c3057f8bd439`,
+        getCertificateCredentials: getAzureCertificateCredentials,
+        webSite: {
+            name: `croquet-australia-admin`,
+            serverFarm: `tim-murphy-west-us`,
+            webSpaceName: `West-US-WestUSwebspace`
+        }
+    };
+}
+
+function getAzureCertificateCredentials(): any {
+    if (!fs.existsSync(this.pem)) {
+        throw new Error(`Cannot find Azure management certificate '${this.pem}'.`)
+    }
+
+    const pemContents = fs.readFileSync(this.pem);
+    const credentials = webSiteManagement.createCertificateCloudCredentials({
+        subscriptionId: this.subscriptionId,
+        pem: pemContents
+    });
+
+    return credentials;
+}
+
+export const config: Config = new Config();
---
+++ scripts/azure/list-websites.ts
@@ -0,0 +1,55 @@
+/** 
+ * List azure websites
+ * 
+ * References:
+ * 
+ *  - http://azure.github.io/azure-sdk-for-node/index.html
+ *  - http://azure.github.io/azure-sdk-for-node/azure-asm-website/latest/
+ */
+
+/// <reference path='../../typings/index.d.ts' />
+
+import { config } from '../../config';
+import { Log } from '../lib/log';
+import { Shell } from '../lib/shell';
+
+const webSiteManagement = require('azure-mgmt-website');
+
+const log = new Log();
+const shell = new Shell(log);
+
+log.info(`Azure Websites`);
+
+log.debug(`Getting certificate credentials`);
+const credentials = config.azure.getCertificateCredentials();
+
+log.debug(`Creating website management client`);
+const client = webSiteManagement.createWebSiteManagementClient(credentials);
+
+log.debug(`Getting list of webspaces...`);
+client.webSpaces.list(function (err, result) {
+    if (err) {
+        throw err;
+    } else {
+        const webSpaces = result.webSpaces;
+
+        webSpaces.forEach(webSpace => {
+            log.info(`  ${webSpace.name}`)
+            client.webSpaces.listWebSites(webSpace.name, function (err, result) {
+                if (err) {
+                    throw err;
+                } else {
+                    const webSites = result.webSites.sort((a, b) => a.name > b.name ? 1 : a.name < b.name ? -1 : 0);
+
+                    webSites.forEach(webSite => {
+                        log.info(`    - ${webSite.name}`);
+                    });
+
+                    // console.log(webSites);
+                }
+            });
+        });
+
+        // console.log(result);
+    }
+});
