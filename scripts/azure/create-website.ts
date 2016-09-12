5acc5fc804f315ffe6fbe97882ef557b3460bf8b
Tim Murphy <tim@26tp.com>
2016-09-13 07:07:36 +1000

    build: add scripts to create Azure website (#5)

---
+++ scripts/azure/create-website.ts
@@ -0,0 +1,38 @@
+/** 
+ * Create azure website
+ * 
+ * References:
+ * 
+ *  - http://azure.github.io/azure-sdk-for-node/index.html
+ *  - http://azure.github.io/azure-sdk-for-node/azure-asm-website/latest/
+ */
+
+/// <reference path='../../typings/index.d.ts' />
+
+import { config } from '../../config';
+import { Log } from '../lib/log';
+import { Shell } from '../lib/shell';
+
+const webSiteManagement = require('azure-mgmt-website');
+
+const log = new Log();
+const shell = new Shell(log);
+
+log.info(`Creating Azure Website`);
+
+log.debug(`Getting certificate credentials`);
+const credentials = config.azure.getCertificateCredentials();
+
+log.debug(`Creating website management client`);
+const client = webSiteManagement.createWebSiteManagementClient(credentials);
+
+client.webSites.create(
+    config.azure.webSite.webSpaceName,
+    config.azure.webSite,
+    (err, result) => {
+        if (err) {
+            throw err;
+        } else {
+            log.info(result);
+        }
+    });
