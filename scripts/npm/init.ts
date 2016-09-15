9d58054a8a9bc730d12d8b8c7e1b2d2e997c1b68
Tim Murphy <tim@26tp.com>
2016-09-16 07:42:07 +1000

    Update to 'angular-cli: 1.0.0-beta.14'

--- scripts/npm/init.ts
+++ scripts/npm/init.ts
@@ -8,9 +8,8 @@ const shell = new Shell(log);
 
 uninstallGlobalNpmPackage('typescript');
 uninstallGlobalNpmPackage('angular-cli');
-cleanNpmCache();
-installGlobalNpmPackage('typescript@2.0.0');    // angular-cli 1.0.0-beta.11-webpack.8 does not work with typescript 2.0.2.
-installGlobalNpmPackage('angular-cli@webpack');
+installGlobalNpmPackage('typescript@2');
+installGlobalNpmPackage('angular-cli@latest');
 
 function uninstallGlobalNpmPackage(packageName: string): void {
     shell.exec(
@@ -18,15 +17,6 @@ function uninstallGlobalNpmPackage(packageName: string): void {
         `Uninstalling global npm package '${log.highlight(packageName)}'...`,
         `Successfully uninstalled '${log.highlight(packageName)}'.`,
         `Cannot uninstall '${log.highlight(packageName)}.`);
-    log.info('');
-}
-
-function cleanNpmCache(): void {
-    shell.exec(
-        `npm cache clean`,
-        `Cleaning npm cache...`,
-        `Successfully cleaned npm cache.`,
-        `Cannot clean npm cache.`);
     log.info('');
 }
 
