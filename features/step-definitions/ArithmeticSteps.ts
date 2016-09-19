02278aa638cadd7218e0e753a572ba8489fc3ff7
Tim Murphy <tim@26tp.com>
2016-09-20 05:40:19 +1000

    Add cucumber examples with npm script 'test:features'

---
+++ features/step-definitions/ArithmeticSteps.ts
@@ -0,0 +1,25 @@
+import { binding, given, then } from "cucumber-tsflow";
+
+@binding()
+class ArithmeticSteps {
+    private computedResult: number;
+
+    @given(/^I enter '(\d+)' and '(\d+)'$/)
+    public givenTwoNumbers(num1: string, num2: string): void {
+        this.computedResult = parseInt(num1) + parseInt(num2);
+    }
+
+    @given(/^I subtract '(\d+)' from '(\d+)'$/)
+    public givenSubtractNumbers(num1: string, num2: string): void {
+        this.computedResult = parseInt(num2) - parseInt(num1);
+    }
+
+    @then(/I receive the result '(\d*)'/)
+    public thenResultReceived(expectedResult: string): void {
+        if (parseInt(expectedResult) !== this.computedResult) {
+            throw new Error("Arithmetic Error");
+        }
+    }
+}
+
+export = ArithmeticSteps;
